5d14befeeb06797151dfe6618df43728bb973f60
NourG8 <nourgerfali08@gmail.com>
2022-02-27 13:00:21 +0100

    Nour_Add_Repository_AnalyseLait

---
+++ PFE_BackEndCollecteur/src/main/java/iset/pfe/example/repositories/AnalyseLaitRepository.java
@@ -0,0 +1,9 @@
+package iset.pfe.example.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import iset.pfe.example.entities.AnalyseLait;
+
+public interface AnalyseLaitRepository extends JpaRepository<AnalyseLait,Integer>{
+
+}
